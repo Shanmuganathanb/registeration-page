f693f81e7bb8fc7b1c272df938ae70b600102050
Shanmuganathanb <38834254+Shanmuganathanb@users.noreply.github.com>
2021-04-01 22:22:49 +0530

    Get registered users from crudcrud

--- index.js
+++ index.js
@@ -21,3 +21,28 @@ Form.addEventListener('submit',(e)=>{
     DateInput.value="";
     TimeInput.value="";
 })
+
+var gotDetails = {}
+async function getData(){
+    var data_1 = await axios.get('https://crudcrud.com/api/50ff5a9e88c24fb7bb28ae626e1d26f7/registeruser');
+    if (data_1.data.length === 0){
+        ul.innerHTML =" No registered users"
+        return
+    }
+    var text = document.createTextNode("User Details");
+    ul.appendChild(text);
+    getBtn.style='display:none;'
+    data_1.data.forEach(user => {
+        var li = document.createElement('li');
+        li.innerHTML = `Name : ${user.name} , Email : ${user.email} , Phone : ${user.phone} , Date : ${user.date} , Time : ${user.time}`
+        
+        ul.append(li)
+    })
+}
+var userDetails = document.getElementById('Users');
+var ul = document.createElement('li');
+ul.style = "list-style-type:none"
+ul.setAttribute('id','userdetails-ul');
+userDetails.appendChild(ul)
+var getBtn = document.querySelector('#getusers');
+getBtn.addEventListener('click',getData);
